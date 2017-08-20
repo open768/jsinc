66ecaf47894c355001a763d8576230c78140bf2f
open seven six ate <open768@yahoo.co.uk>
2017-08-20 19:41:47 +0100

    created useful constants

--- ck-inc/space/curiosity.js
+++
@@ -1,4 +0,0 @@
-var cMission = {
-	name:"curiosity",
-	ID: "MSL"
-};
---
+++ ck-inc/space/space.js
@@ -0,0 +1,19 @@
+var cMission = {
+	name:"curiosity",
+	ID: "MSL"
+};
+
+var cSpaceBrowser = {
+	SOL_QUERYSTRING:"s",
+	INSTR_QUERYSTRING:"i",
+	PRODUCT_QUERYSTRING:"p",
+	DATE_QUERYSTRING:"t",
+	THUMB_QUERYSTRING: "t",
+	TIMESTAMP_QUERYSTRING: "t",
+	BEGIN_QUERYSTRING:"b",
+	MOSAIC_QUERYSTRING: "mos",
+	SHEET_QUERYSTRING: "sheet",
+	OUTPUT_QUERYSTRING: "o",
+
+	ALL_INSTRUMENTS: "All"
+}
