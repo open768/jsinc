a3dda5fecf289ed820a5a6df52182aaf4e1e8a98
open seven six ate <open768@yahoo.co.uk>
2017-12-02 19:56:28 +0000

    added binary converter

---
+++ ck-inc/converter.js
@@ -0,0 +1,64 @@
+var cConverterEncodings = {
+	binary:"01",
+	BASE64:"0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ+/=",
+
+	isBase64:function(psInput){
+		for (var i=0 ; i<psInput.length; i++){
+			var ch = psInput.charAt(i);
+			if (this.BASE64.indexOf(ch) == -1){
+				cDebug.write("invalid char:"+ch);
+				return false;
+			}
+		}
+		return true;
+	},
+}
+var cConverter = {
+	//*********************************************************************
+	binToInt:function(psBin){
+		var iVal = 0;
+		var bFirst = true;
+		for (var i = psBin.length; i>0; i--){
+			var ch = psBin.charAt(i-1);
+			if (!bFirst){
+				iVal = iVal << 1;
+			}
+			if (ch == '1'){
+				iVal = iVal | 1;
+			}
+			bFirst = false;
+		}
+		return iVal;
+	},
+	
+	//*********************************************************************
+	intToBin:function(piVal){
+		var iVal = piVal;
+		var sBin = ""
+		
+		while (iVal > 0){
+			sBin = sBin + ((iVal & 1)==1?"1":"0");
+			iVal = iVal >>> 1;
+		}
+		return sBin;
+	},
+	
+	
+	//**********************************
+	test: function(){
+		cDebug.write("testing cConverter")
+		var iMax32Int = Math.pow(2,32) - 1;
+		cDebug.write("- maxint is:"+ iMax32Int );
+		var iRand = Math.floor(Math.random() * iMax32Int);
+		cDebug.write("- random number in:"+ iRand);
+		var sBin = this.intToBin(iRand);
+		cDebug.write("- binary out :"+ sBin);
+		var i32 = this.binToInt(sBin);
+		cDebug.write("- number out :"+ i32);
+		if (i32 !== iRand) throw new Error("cConverter test failed")
+		
+		cDebug.write("cConverter test passed :-)");
+		return true;
+	}
+}
+
