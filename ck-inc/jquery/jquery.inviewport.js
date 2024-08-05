bfa1c1b3876d6e16725a81e87d51bb70b2ada7b3
sunil <cluck@chickenkatsu.co.uk>
2024-08-05 12:39:13 +0100

    eslint changes

--- ck-inc/jquery/jquery.inviewport.js
+++ ck-inc/jquery/jquery.inviewport.js
@@ -1,4 +1,4 @@
-;(function ($) {
+(function ($) {
 	/**************************************************************************
 	Copyright (C) Chicken Katsu 2013-2024
 	 * Licensed under the MIT license.
