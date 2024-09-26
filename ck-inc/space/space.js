3389e08bac437d135e8b06f2f5a17f2f7911d9da
sunil <cluck@chickenkatsu.co.uk>
2024-09-26 17:05:32 +0100

    renamed constants

--- ck-inc/space/space.js
+++ ck-inc/space/space.js
@@ -6,17 +6,17 @@ Copyright (C) Chicken Katsu 2013 - 2024
 **************************************************************************/
 
 class cSpaceBrowser {
-	static SOL_QUERYSTRING = 's'
-	static INSTR_QUERYSTRING = 'i'
-	static PRODUCT_QUERYSTRING = 'p'
-	static DATE_QUERYSTRING = 't'
-	static THUMB_QUERYSTRING = 't'
-	static TIMESTAMP_QUERYSTRING = 't'
-	static BEGIN_QUERYSTRING = 'b'
-	static MOSAIC_QUERYSTRING = 'mos'
-	static SHEET_QUERYSTRING = 'sheet'
-	static OUTPUT_QUERYSTRING = 'o'
-	static MISSION_QUERYSTRING = 'm'
+	static SOL_PARAM = 's'
+	static INSTR_PARAM = 'i'
+	static PRODUCT_PARAM = 'p'
+	static DATE_PARAM = 't'
+	static THUMB_PARAM = 't'
+	static TIMESTAMP_PARAM = 't'
+	static BEGIN_PARAM = 'b'
+	static MOSAIC_PARAM = 'mos'
+	static SHEET_PARAM = 'sheet'
+	static OPERATION_PARAM = 'o'
+	static MISSION_PARAM = 'm'
 
 	static ALL_INSTRUMENTS = 'All'
 }
