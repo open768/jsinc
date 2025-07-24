97391758da84435f50c145fc18fc5c499e1d07c5
sunil <cluck@chickenkatsu.co.uk>
2025-07-24 23:03:46 +0100

    various

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,34 @@
-// eslint.config.js
-import globals from "globals"
-import js from "@eslint/js"
+import globals from "globals";
+import js from "@eslint/js";
+import { defineConfig, globalIgnores } from "eslint/config";
 
-export default [
-	js.configs.recommended,
-	{
-		languageOptions: {
-			globals: {
-				...globals.jquery,
-				...globals.browser,
-				bean: "readonly",
-				cAppLocations: "readonly",
-				cAuth: "readonly",
-				cBrowser: "readonly",
-				cCommon: "readonly",
-				cDebug: "readonly",
-				cDetail: "readonly",
-				cHttp2: "readonly",
-				cHttpQueue: "readonly",
-				cHttpQueueItem: "readonly",
-				cJquery: "readonly",
-				cMission: "readonly",
-				cQueue: "readonly",
-				cSecret: "readonly"
-			}
-		},
-		rules: {
-			"no-unused-vars": "warn",
-			semi: "off"
-		},
-		ignores: ["**\\extra\\"]
-	}
-]
+export default defineConfig([
+  globalIgnores(["extra/**"]),
+  js.configs.recommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.jquery,
+        ...globals.browser,
+        bean: "readonly",
+        cAppLocations: "readonly",
+        cAuth: "readonly",
+        cBrowser: "readonly",
+        cCommon: "readonly",
+        cDebug: "readonly",
+        cDetail: "readonly",
+        cHttp2: "readonly",
+        cHttpQueue: "readonly",
+        cHttpQueueItem: "readonly",
+        cJquery: "readonly",
+        cMission: "readonly",
+        cQueue: "readonly",
+        cSecret: "readonly",
+      },
+    },
+    rules: {
+      "no-unused-vars": "warn",
+      semi: "off",
+    },
+  },
+]);
