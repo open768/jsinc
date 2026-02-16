c33a2340573545d1811b205bdea498b1acf4b4bb
sun-van-work <sunil.vanmullem@cgi.com>
2026-02-16 14:23:52 +0000

    removed "grid" from event

--- ck-inc/event.js
+++ ck-inc/event.js
@@ -1,6 +1,6 @@
 //***************************************************************************
 class cBaseEvent {
-	base_id = null //base ID allows consumers to listen for a specific event
+	base_id = null //allows consumers to listen for events associated with a common base_id
 	action = null
 	data = null
 	/**
@@ -11,26 +11,25 @@ class cBaseEvent {
 
 	/**
 	 * Creates a new CA event instance.
-	 \*	 * @param {string} psBaseId - The base ID associated with the event.
+	 * @param {string} psBaseId - The base ID associated with the event.
 	 * @param {string} psAction - The action type for the event.
 	 * @param {*} [poData=null] - Optional payload for the event.
 	 * @throws {Error} If required arguments are missing or the class does not override event_type_id.
 	 */
 	constructor(psBaseId, psAction, poData = null) {
-		if (!psBaseId || !psAction) 
-			$.error('incorrect number of arguments')
-		
 		if (typeof bean === 'undefined') 
 			$.error('bean library is missing')
-		
-
 		if (this.constructor === cBaseEvent) 
 			$.error('cBaseEvent is abstract')
-		
 		// @ts-expect-error
 		if (!this.constructor.event_type_id) 
 			$.error('event_type_id not overridden in class:' + this.constructor.name)
 		
+		if (!psBaseId ) 
+			$.error('base ID missing')
+		if (!psAction) 
+			$.error('action missing')
+
 
 		this.base_id = psBaseId
 		this.action = psAction
@@ -43,42 +42,41 @@ class cBaseEvent {
 	 */
 	channel_id() {
 		// @ts-expect-error
-		return this.constructor.event_type_id + this.base_id //creates a unique ID for a specific grid
+		return this.constructor.event_type_id + this.base_id //creates a unique ID for a specific event and base
 	}
 
 	async trigger() {
 		var sEventName = this.channel_id()
-		cDebug.write('event>> grid:"' + this.base_id + '" type:' + this.constructor.name + ' action:' + this.action)
+		cDebug.write('event>> base:"' + this.base_id + '" type:' + this.constructor.name + ' action:' + this.action)
 		bean.fire(document, sEventName, this)
 	}
 
-	static async fire_event(psGridName, psAction, poData = null) {
+	static async fire_event(psBaseId, psAction, poData = null) {
 		if (this === cBaseEvent) 
 			throw new CAException('cBaseEvent is abstract')
 		
-		if (!psGridName) 
-			$.error('grid name is required')
+		if (!psBaseId) 
+			$.error('base ID is required')
 		
 		if (!psAction) 
-			$.error('grid name is required')
+			$.error('action is required')
 		
 
-		var oEvent = new this(psGridName, psAction, poData) //create specific instance
+		var oEvent = new this(psBaseId, psAction, poData) //create specific instance
 		oEvent.trigger()
 	}
 
-	static async subscribe(psGridName, pfnCallback) {
+	static async subscribe(psBaseId, pfnCallback) {
 		if (this === cBaseEvent) 
 			$.error('cBaseEvent is abstract')
 		
 		if (typeof pfnCallback !== 'function') 
 			$.error('callback must be a function')
 		
-		if (!psGridName) 
-			$.error('grid name is required')
-		
+		if (!psBaseId) 
+			$.error('base ID is required')
 
-		var oEvent = new this(psGridName, 'dummy') //create an event to get the channel ID
+		var oEvent = new this(psBaseId, 'dummy') //create an event to get the channel ID
 		bean.on(document, oEvent.channel_id(), pfnCallback)
 	}
 }
