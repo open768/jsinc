ef67befb0975b0ae183ecacaf37ef8254c8c0e20
sun-van-work <sunil.vanmullem@cgi.com>
2026-03-05 10:17:13 +0000

    merge suppress into debug.js

--- ck-inc/debug.js
+++ ck-inc/debug.js
@@ -22,6 +22,53 @@ class cDebugTypes {
 	}
 }
 
+class cSuppressMessage{
+	static _counts = {}
+	static _timestamps = {}
+
+	static NOISY_COUNT = 50 //how many times a message can be logged before it is considered noisy
+	static NOISY_RESET = 200	//milliseconds
+
+
+	/**
+     * @param {string} psThing
+     * @returns {boolean}
+     */
+	static should_suppress(psThing){
+
+		//-----------------update the count
+		var aCounts = this._counts
+		var iCount = aCounts[psThing] || 0
+		iCount++
+		aCounts[psThing] = iCount
+
+		// ----- its not noisy
+		if (iCount <= this.NOISY_COUNT)
+			return false
+
+		// ----- its just become noisy
+		if (iCount === this.NOISY_COUNT){
+			var iNow = new Date().getTime()
+			this._timestamps[psThing] = iNow
+			cDebug.write('suppressing message "' + psThing + '"')
+			return true
+		}
+
+		// ----- its already noisy - check if its been long enough to reset the count
+		var iNow = new Date().getTime()
+		var aTimestamps = this._timestamps
+		var iLastTimestamp = aTimestamps[psThing] || iNow
+		var idiff = iNow - iLastTimestamp
+
+		if (idiff > this.NOISY_RESET){
+			aCounts[psThing] = 0
+			aTimestamps[psThing] = null
+			return false
+		}
+
+		return true
+	}
+}
 
 class cDebug {
 	static DEBUGGING = false
@@ -80,9 +127,8 @@ class cDebug {
 	//*****************************************************
 	static write(psMessage, piLevel = cDebugTypes.levels.off) {
 		if (this.SUPPRESSION )
-			if (typeof cSuppressMessage !== 'undefined')
-				if (cSuppressMessage.should_suppress(psMessage))
-					return
+			if (cSuppressMessage.should_suppress(psMessage))
+				return
 
 		if (this.DEBUGGING || this.ONE_TIME_DEBUGGING)
 			if (this.level >= piLevel) {
