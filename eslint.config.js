3e3c8aae7a1c54f92cf35b7c8a1159063dc765a1
sun-van-work <sunil.vanmullem@cgi.com>
2026-03-13 09:59:48 +0000

    eslint

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@
 import { defineConfig } from "eslint/config"
 // @ts-ignore
 import stylistic from "@stylistic/eslint-plugin"
+// @ts-ignore
+import jsdoc from "eslint-plugin-jsdoc"
 
 export default defineConfig([
 	{
@@ -20,6 +22,7 @@ export default defineConfig([
 		},
 		plugins: {
 			"@stylistic": stylistic,
+			jsdoc,
 		},
 		...stylistic.configs.recommended,
 		rules: {
@@ -63,6 +66,7 @@ export default defineConfig([
 			"@stylistic/brace-style": ["error", "1tbs", { "allowSingleLine": false }], // [5](https://eslint.style/rules/default/brace-style)
 			// Enforce consistent indentation (auto-fixable)
 			"@stylistic/indent": ["error", "tab", { "SwitchCase": 1 }], // [6](https://eslint.style/rules/default/indent)
+			"jsdoc/check-alignment": "error",
 		},
 	},
 ])
